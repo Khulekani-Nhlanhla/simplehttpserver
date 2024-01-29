a5db8dd859b2eeeb0c877e5bc4ef1083bce7aa5f
Kxxle <64878567+Kxxle@users.noreply.github.com>
2024-01-29 18:14:17 +0100

    I have added a ObjectMapper object. I then added a default method for the object mapper. I then created a parse method that returns a json node. I then added a from and to json method.

--- src/main/java/com/khulekani/httpserver/util/Json.java
+++ src/main/java/com/khulekani/httpserver/util/Json.java
@@ -1,5 +1,33 @@
 package com.khulekani.httpserver.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
 public class Json {
+
+    private static ObjectMapper myObjectMapper;
+
+    private static ObjectMapper defaultObjectMapper(){
+        ObjectMapper om = new ObjectMapper();
+        om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return om;
+    }
+
+    public static JsonNode parse(String jsonSrc) throws IOException {
+        return myObjectMapper.readTree(jsonSrc);
+    }
+
+    public static <A> A fromJson(JsonNode node, Class<A> clazz) throws JsonProcessingException {
+        return myObjectMapper.treeToValue(node, clazz);
+    }
+
+    public static JsonNode toJson(Object obj){
+        return myObjectMapper.valueToTree(obj);
+    }
+
 
 }
