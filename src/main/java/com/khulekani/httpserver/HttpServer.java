fb16d3907fab3e5e8d1870f2fe4cd50dd565594d
Kxxle <64878567+Kxxle@users.noreply.github.com>
2024-02-04 18:11:15 +0100

    I changed the printed text and started the ConfigurationManager.

--- src/main/java/com/khulekani/httpserver/HttpServer.java
+++ src/main/java/com/khulekani/httpserver/HttpServer.java
@@ -1,10 +1,14 @@
 package com.khulekani.httpserver;
+
+import com.khulekani.httpserver.config.ConfigurationManager;
+
 /*
 * Driver Class for the HTTP server
 * */
 public class HttpServer {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Server Starting....");
 
+        ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
     }
 }
