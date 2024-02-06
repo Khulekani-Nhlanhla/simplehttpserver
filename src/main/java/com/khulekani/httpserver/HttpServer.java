fa4899d8127a8aa0e6a7b41aebe1301e702eee31
Kxxle <64878567+Kxxle@users.noreply.github.com>
2024-02-06 15:48:29 +0100

    I replaced the print statements to logger info displayed in the console.

--- src/main/java/com/khulekani/httpserver/HttpServer.java
+++ src/main/java/com/khulekani/httpserver/HttpServer.java
@@ -3,6 +3,8 @@ package com.khulekani.httpserver;
 import com.khulekani.httpserver.config.Configuration;
 import com.khulekani.httpserver.config.ConfigurationManager;
 import com.khulekani.httpserver.core.ServerListenerThread;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,14 +16,16 @@ import java.net.Socket;
 * Driver Class for the HTTP server
 * */
 public class HttpServer {
-    public static void main(String[] args) {
-        System.out.println("Server Starting....");
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpServer.class);
+    public static void main(String[] args) {
+
+        LOGGER.info("Server Starting....");
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
         Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
+        LOGGER.info("Using Port: " + conf.getPort());
+        LOGGER.info("Using WebRoot: " + conf.getWebroot());
 
-        System.out.println("Using Port: " + conf.getPort());
-        System.out.println("Using WebRoot: " + conf.getWebroot());
 
         try {
             ServerListenerThread serverListenerThread = new ServerListenerThread(conf.getPort(),conf.getWebroot());
