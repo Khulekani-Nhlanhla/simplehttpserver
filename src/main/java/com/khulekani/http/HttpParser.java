24ee1c328f51053c63da359f9c280fc02937a1c3
Kxxle <64878567+Kxxle@users.noreply.github.com>
2024-02-12 16:49:37 +0100

    I added a scannerless parser. This is to make sure that errors can be detected in a socket as the data is processed.I also included the Junit dependency to test the parser.

---
+++ src/main/java/com/khulekani/http/HttpParser.java
@@ -0,0 +1,17 @@
+package com.khulekani.http;
+
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+import java.io.InputStream;
+
+
+public class HttpParser {
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpParser.class);
+
+    public void parseHttpRequest(InputStream inputStream){
+
+    }
+
+}
