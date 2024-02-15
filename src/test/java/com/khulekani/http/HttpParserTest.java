d22eca32856a459528149cdb5c23e3ce48d764b0
Kxxle <64878567+Kxxle@users.noreply.github.com>
2024-02-15 14:41:13 +0100

    I added the text from the Request.txt into the InputStream in the GenerateValidTestCase.

--- src/test/java/com/khulekani/http/HttpParserTest.java
+++ src/test/java/com/khulekani/http/HttpParserTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+
 import static org.junit.jupiter.api.Assertions.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class HttpParserTest {
@@ -15,6 +20,35 @@ class HttpParserTest {
     }
     @Test
     void parseHttpRequest() {
+        httpParser.parseHttpRequest(
+                GenerateValidTestCase()
+        );
+    }
+    private InputStream GenerateValidTestCase(){
+        String rawData = "GET / HTTP/1.1\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Connection: keep-alive\r\n" +
+                "sec-ch-ua: \"Not\r\n" +
+                "A(Brand\";v=\"99\", \"Brave\";v=\"121\", \"Chromium\";v=\"121\"\r\n" +
+                "sec-ch-ua-mobile: ?0\r\n" +
+                "sec-ch-ua-platform: \"Windows\"\r\n" +
+                "Upgrade-Insecure-Requests: 1\r\n" +
+                "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36\r\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8\r\n" +
+                "Sec-GPC: 1\r\n" +
+                "Accept-Language: en-GB,en\r\n" +
+                "Sec-Fetch-Site: none\r\n" +
+                "Sec-Fetch-Mode: navigate\r\n" +
+                "Sec-Fetch-User: ?1\r\n" +
+                "Sec-Fetch-Dest: document\r\n" +
+                "Accept-Encoding: gzip, deflate,br"+"\r\n";
 
+        InputStream inputStream = new ByteArrayInputStream(
+                rawData.getBytes(
+                        StandardCharsets.US_ASCII
+                )
+        );
+
+        return inputStream;
     }
 }
